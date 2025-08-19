49eadb944a323ec8fbef02d43e97d890fa661b87
Paul Fontes <pgfon22@gmail.com>
2025-08-19 14:20:29 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// 🏪💾 DATA (State) ----------------------------------------
+    let shopItems = [
+        { 
+        name : "Vanilla",
+        price : 4,
+        quantity : 0
+        }
+        { 
+        name : "Chocolate",
+        price : 5,
+        quantity : 0
+        }
+        { 
+        name : "Sprinkles",
+        price : 2,
+        quantity : 0
+        }
+        { 
+        name : "Chocolate Chips",
+        price : 3,
+        quantity : 0
+        }
+    ]
+
+
+
+
+
+
+
+
+
+// 🧠⚡ BUSINESS LOGIC (functions that modify the state)------
+
+
+
+
+
+
+
+
+
+
+
+// 🎨🖌️ VISUALIZERS (functions that draw data from the state to the page)
